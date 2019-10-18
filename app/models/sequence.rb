ac1359b2ccec4d3593c09396af7f33ef3ab7498f
Eric Franz <efranz@osc.edu>
2019-10-18 13:59:18 -0400

    add geo methods to Sequence

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -1,13 +1,4 @@
 class Sequence < ActiveRecord::Base
-  # FIXME: this causes crash :-(
-  #
-  # alias_attribute :gbifID, :gbif_id
-  # alias_attribute :decimalLatitude, :lat
-  # alias_attribute :decimalLongitude, :lng
-
-  # # currently ignoring subgenus
-  # %w(kingdom phylum class order family genus species).each do |taxon|
-  #   alias_attribute taxon.to_sym, "taxon_#{taxon}".to_sym
-  # end
+  acts_as_mappable
 end
 
