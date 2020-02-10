b442af0cec5fdde549c1346f90c10009a58fce7d
Eric Franz <efranz@osc.edu>
2020-02-10 09:37:55 -0500

    refactor out expand_gbif_occurrences_on_accession

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -34,6 +34,9 @@ class Pipeline:
         self.genbank_path = genbank_path
         self.output_dir = output_dir
 
+    def expanded_occurrence_part_path(self):
+        return os.path.join(self.output_dir, os.path.basename(self.genbank_path) + ".gbif");
+
     def index_path(self):
         return os.path.join(self.output_dir, os.path.basename(self.genbank_path) + ".idx");
 
@@ -41,12 +44,12 @@ class Pipeline:
         """Create if doesn't exist, then return BioPython flatfile index"""
         return SeqIO.index_db(self.index_path(), self.genbank_path, 'genbank', None, accession_from_version)
 
-    def write_expanded_occurrence_record(self, accession, parts, record):
+    def write_expanded_occurrence_record(self, gbif_out_file, accession, parts, record):
         # add columns:
         # 1. alt organism if genbank organism differs from gbif
         # 2. genbank file source (basename self.genbank_path)
         # [accession] + clean(parts)[1:-1] + [alt organism] + [genbank]
-        pass
+        gbif_out_file.write("\t".join([accession] + parts[1:]))
 
     def write_gene_sequence_data(self):
         # create directory to store sequence data
@@ -58,25 +61,35 @@ class Pipeline:
         # write gene metadata record to file:   genes.txt.part.X
         pass
 
+    def expand_gbif_occurrences_on_accession(self, gbif_file, db, gbif_out_file, postprocess = None):
+        accessions_postprocessed = set()
+        accession_regex = re.compile('\w{2}\d{6}')
+
+        for line in gbif_file:
+            # refactor to each_accession after move to class
+            parts = line.split("\t")
+            for accession in accession_regex.findall(parts[0]):
+                record = db.get(accession)
+                if record:
+                    self.write_expanded_occurrence_record(gbif_out_file, accession, parts, record)
+
+                    if postprocess and not accession in accessions_postprocessed:
+                        postprocess(accession)
+                        accessions_postprocessed.add(record, accession)
+
+    def write_gene_data(self, record, accession):
+        genes = genes_for_record(record, accession)
+        gene_lengths = [g.length() for g in genes]
+
+        # TODO: for each gene, write sequence data and write gene metadata
+
+        return max(gene_lengths)
+
     def pipeline(self):
         db = self.make_index()
 
-        # maintain a set of accessions HERE
-        sequences_written = set()
+        # max_gene_length = 0
+        with open(self.gbif_path, 'r') as gbif_file, open(self.expanded_occurrence_part_path(), 'w') as gbif_out_file:
+            self.expand_gbif_occurrences_on_accession(gbif_file, db, gbif_out_file, lambda r, a: self.write_gene_data(r,a))
 
-        accession_regex = re.compile('\w{2}\d{6}')
-
-        with open(self.gbif_path) as gbif:
-            for line in gbif:
-                # refactor to each_accession after move to class
-                parts = line.split("\t")
-                for accession in accession_regex.findall(parts[0]):
-                    record = db.get(accession)
-                    if record:
-                        write_expanded_occurrence_record(accession, parts, record)
-
-                        if not accession in sequences_written:
-                            self.write_gene_sequence_data()
-                            self.write_gene_metatada_record()
-
-                            sequences_written.add(accession)
+        # print(f'max_gene_length: {max_gene_length}')
--- pipeline/test_pipeline.py
+++ pipeline/test_pipeline.py
@@ -1,4 +1,7 @@
 import unittest
+from io import StringIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 import pipeline
 from pipeline import Pipeline
 
@@ -11,6 +14,28 @@ class TestPipeline(unittest.TestCase):
     def test_index_path(self):
         self.assertEqual('gbvrt94.seq.idx', Pipeline('', 'gbvrt94.seq', '').index_path())
         self.assertEqual('/out/gbvrt94.seq.idx', Pipeline('', '/genbank/files/gbvrt94.seq', '/out').index_path())
+
+    def test_expand_gbif_occurrences_on_accession(self):
+        # TODO: add taxonomoy, and test for alternate organism, genbank source file, etc.
+        tsv = "https://www.ncbi.nlm.nih.gov/nuccore/MG076766\t1841684417\t43.37\t-80.36\n" + \
+              "https://www.ncbi.nlm.nih.gov/nuccore/KT604601| other\t1841684418\t54.76\t-126.93\n" + \
+              "https://www.ncbi.nlm.nih.gov/nuccore/KT605555| other\t1841684419\t56.26\t-124.33\n"
+        expected_tsv = "MG076766\t1841684417\t43.37\t-80.36\n" + \
+              "KT604601\t1841684418\t54.76\t-126.93\n"
+        db = {
+            "MG076766" : SeqRecord(Seq("ATGC")),
+            "KT604601" : SeqRecord(Seq("ATGC"))
+        }
+        out = StringIO()
+
+        Pipeline('', '', '').expand_gbif_occurrences_on_accession(StringIO(tsv), db, out)
+
+        self.assertEqual(expected_tsv, out.getvalue())
+
+        # Animalia        Arthropoda      Insecta Diptera Chironomidae    \N      \N      \N
+        # good example of a bad use case => there is no species specified though we could fill in the gaps
+        # with the accession; or for the accession, get the taxid, and use that to get the full blessed
+        # taxonomy => which I think is preferable
 
 if __name__ == '__main__':
     unittest.main()
