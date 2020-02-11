49ed9d46cb1bdb4d5ed078fa5f5e9773a6197056
Eric Franz <efranz@osc.edu>
2020-02-11 16:02:13 -0500

    fix gbif processing to write out gbsource and alt organism

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -53,8 +53,10 @@ class Gene:
 def genes_for_record(record, accession):
     return [Gene(f, record, accession) for f in record.features if (f.type == 'CDS' and ('gene' in f.qualifiers or 'product' in f.qualifiers)) ]
 
+
 def accession_from_version(version):
     return version.split('.')[0]
+
 
 class Pipeline:
     def __init__(self, gbif_path, genbank_path, output_dir):
@@ -62,11 +64,12 @@ class Pipeline:
         self.genbank_path = genbank_path
         self.output_dir = output_dir
 
-    def expanded_occurrence_part_path(self):
-        return os.path.join(self.output_dir, os.path.basename(self.genbank_path) + ".gbif");
+        self.output_occurrences_path = os.path.join(self.output_dir, os.path.basename(self.genbank_path) + ".gbif.tsv");
+        self.output_genes_path = os.path.join(self.output_dir, os.path.basename(self.genbank_path) + ".genes.tsv");
 
     def index_path(self):
         return os.path.join(self.output_dir, os.path.basename(self.genbank_path) + ".idx");
+
 
     def make_index(self):
         """Create if doesn't exist, then return BioPython flatfile index"""
@@ -87,12 +90,14 @@ class Pipeline:
 
         return alt
 
-    def write_expanded_occurrence_record(self, gbif_out_file, accession, parts, record):
-        # add columns:
-        # 1. alt organism if genbank organism differs from gbif
-        # 2. genbank file source (basename self.genbank_path)
-        # [accession] + clean(parts)[1:-1] + [alt organism] + [genbank]
-        gbif_out_file.write("\t".join([accession] + parts[1:]))
+    def write_occurrence_record(self, gbif_out_file, occurrence_, record):
+        """append to end of occurrence source genbank filename and alt organism string"""
+        occurrence = occurrence_without_null(occurrence_)
+
+        gbif_out_file.write("\t".join(occurrence
+         + [os.path.basename(self.genbank_path)]
+         + [alt_species(occurrence, record)])
+        )
 
     def write_gene_sequence_data(self):
         # create directory to store sequence data
@@ -104,21 +109,21 @@ class Pipeline:
         # write gene metadata record to file:   genes.txt.part.X
         pass
 
-    def expand_gbif_occurrences_on_accession(self, gbif_file, db, gbif_out_file, postprocess = None):
+    def filter_gbif_occurrences_on_accession(self, gbif_file, db, gbif_out_file, postprocess = None):
         accessions_postprocessed = set()
-        accession_regex = re.compile('\w{2}\d{6}')
 
         for line in gbif_file:
             # refactor to each_accession after move to class
-            parts = line.split("\t")
-            for accession in accession_regex.findall(parts[0]):
-                record = db.get(accession)
-                if record:
-                    self.write_expanded_occurrence_record(gbif_out_file, accession, parts, record)
+            occurrence = line.split("\t")
+            accession = occurrence[OccurrenceRecordIndex.ACCESSION]
+            record = db.get(accession)
+            if record:
+                self.write_occurrence_record(gbif_out_file, occurrence, record)
 
-                    if postprocess and not accession in accessions_postprocessed:
-                        postprocess(accession)
-                        accessions_postprocessed.add(record, accession)
+                # post process only once per accession
+                if postprocess and not accession in accessions_postprocessed:
+                    postprocess(record, accession)
+                    accessions_postprocessed.add(accession)
 
     def write_gene_data(self, record, accession):
         genes = genes_for_record(record, accession)
@@ -132,7 +137,7 @@ class Pipeline:
         db = self.make_index()
 
         # max_gene_length = 0
-        with open(self.gbif_path, 'r') as gbif_file, open(self.expanded_occurrence_part_path(), 'w') as gbif_out_file:
-            self.expand_gbif_occurrences_on_accession(gbif_file, db, gbif_out_file, lambda r, a: self.write_gene_data(r,a))
+        with open(self.gbif_path, 'r') as gbif_file, open(self.output_occurrences_path, 'w') as gbif_out_file:
+            self.filter_gbif_occurrences_on_accession(gbif_file, db, gbif_out_file, self.write_gene_data)
 
         # print(f'max_gene_length: {max_gene_length}')
