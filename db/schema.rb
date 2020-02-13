217ecef11a6e5721f5cebd3902a9b52223f37afc
Eric Franz <efranz@osc.edu>
2020-02-12 20:00:45 -0500

    Revert "add gene_abbrev and taxon_subspecies"
    This reverts commit 489e85634afe96acad15e7cc8d4186c96803ed09.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200121011509) do
+ActiveRecord::Schema.define(version: 20191018010553) do
 
   create_table "sequences", force: :cascade do |t|
     t.string   "gbif_id"
@@ -31,8 +31,6 @@ ActiveRecord::Schema.define(version: 20200121011509) do
     t.text     "sequence_aligned"
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.string   "taxon_subspecies"
-    t.string   "gene_abbrev"
   end
 
 end
