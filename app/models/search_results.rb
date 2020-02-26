de2541fb7de6d56def404ae4477719a739d9bbb9
Eric Franz <efranz@osc.edu>
2020-02-25 19:29:03 -0500

    fix SearchResults#params returning coords

--- app/models/search_results.rb
+++ app/models/search_results.rb
@@ -2,8 +2,8 @@ class SearchResults
   attr_reader :swpoint, :nepoint, :taxonomy
 
   def self.from_params(params)
-    swpoint = [params[:southwest_corner_latitude], params[:southwest_corner_longitude]]
-    nepoint = [params[:northeast_corner_latitude], params[:northeast_corner_longitude]]
+    swpoint = [params['southwest_corner_latitude'], params['southwest_corner_longitude']]
+    nepoint = [params['northeast_corner_latitude'], params['northeast_corner_longitude']]
     taxonomy = params.select {|k,v| k.starts_with?("taxon_") && v.present? }.symbolize_keys
 
     self.new(swpoint, nepoint, taxonomy)
