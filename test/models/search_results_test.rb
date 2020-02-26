a2a35a985236a6f6b386bd0e08053953aadcab42
Eric Franz <efranz@osc.edu>
2020-02-25 20:17:36 -0500

    show results summary page with only # seqs

--- test/models/search_results_test.rb
+++ test/models/search_results_test.rb
@@ -1,29 +1,30 @@
 require 'test_helper'
 
 class SearchResultsTest < ActiveJob::TestCase
-  test "summary includes correct fasta lengths" do
-    fa1 = genes(:obsoletus1).to_fasta.length + genes(:obsoletus2).to_fasta.length
-    fa2 = genes(:vulpinus).to_fasta.length
+  # FIXME: these tests failed to catch problems when using MySQL
+  # test "summary includes correct fasta lengths" do
+  #   fa1 = genes(:obsoletus1).to_fasta.length + genes(:obsoletus2).to_fasta.length
+  #   fa2 = genes(:vulpinus).to_fasta.length
 
-    swpoint = [29, -110]
-    nepoint = [45, -73]
+  #   swpoint = [29, -110]
+  #   nepoint = [45, -73]
 
-    summary = SearchResults.new(swpoint, nepoint, {}).summary
+  #   summary = SearchResults.new(swpoint, nepoint, {}).summary
 
-    assert_equal fa1, summary[0]["fa_length"]
-    assert_equal fa2, summary[1]["fa_length"]
-  end
+  #   assert_equal fa1, summary[0]["fa_length"]
+  #   assert_equal fa2, summary[1]["fa_length"]
+  # end
 
-  test "summary includes correct aligned fasta lengths" do
-    fa1 = genes(:obsoletus1).to_aligned_fasta.length + genes(:obsoletus2).to_aligned_fasta.length
-    fa2 = genes(:vulpinus).to_aligned_fasta.length
+  # test "summary includes correct aligned fasta lengths" do
+  #   fa1 = genes(:obsoletus1).to_aligned_fasta.length + genes(:obsoletus2).to_aligned_fasta.length
+  #   fa2 = genes(:vulpinus).to_aligned_fasta.length
 
-    swpoint = [29, -110]
-    nepoint = [45, -73]
+  #   swpoint = [29, -110]
+  #   nepoint = [45, -73]
 
-    summary = SearchResults.new(swpoint, nepoint, {}).summary
+  #   summary = SearchResults.new(swpoint, nepoint, {}).summary
 
-    assert_equal fa1, summary[0]["afa_length"]
-    assert_equal fa2, summary[1]["afa_length"]
-  end
+  #   assert_equal fa1, summary[0]["afa_length"]
+  #   assert_equal fa2, summary[1]["afa_length"]
+  # end
 end
