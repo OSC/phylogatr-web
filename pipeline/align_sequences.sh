8b612c568474913b46900631685030e825966c05
Eric Franz <efranz@osc.edu>
2020-06-04 21:42:13 -0400

    add job script to do alignments

--- pipeline/align_sequences.sh
+++ pipeline/align_sequences.sh
@@ -3,6 +3,22 @@
 set -xe
 set -o pipefail
 
-time ./db_to_fasta_file.rb $1 | ../bin/muscle3.8.31_i86linux64 -quiet | ../bin/trimal -in /dev/stdin | ./aligned_fasta_to_db.rb
+# $1 is path to fasta file ending in .fa
+fasta_file=$1
+fasta_file_aligned="${fasta_file%.fa}.afa"
+fasta_tmp="$TMPDIR/$(basename $fasta_file_aligned)"
 
+function remove_newlines_in_fasta {
+  ruby -ne 'print $_[0] == ">" ? "\n#{$_}" : $_.sub("\n","")' | ruby -e 'puts $stdin.read.strip'
+}
 
+function align {
+  ../bin/mafft --adjustdirection --auto --inputorder --quiet $1
+}
+
+function trim {
+  ../bin/trimal -in $1 -resoverlap 0.85 -seqoverlap 50 -gt 0.15
+}
+
+align $fasta_file > $fasta_tmp
+trim $fasta_tmp | remove_newlines_in_fasta > $fasta_file_aligned
