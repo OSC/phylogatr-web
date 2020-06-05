0f4f3c486cb0c5c876a14a5abe0e386e2bc19df6
Eric Franz <efranz@osc.edu>
2020-06-04 21:49:50 -0400

    for now, avoid importing genes into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
 Dir.chdir('/fs/scratch/PAS1604/genbank') do
   ActiveRecord::Base.connection.execute('load data local infile "gbif_occurrences_final.tsv" into table occurrences;')
-  ActiveRecord::Base.connection.execute('load data local infile "genes.tsv" into table genes;')
+  # ActiveRecord::Base.connection.execute('load data local infile "genes.tsv" into table genes;')
 end
