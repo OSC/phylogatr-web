79d3c2cba8044cef8f8c711507c0307b1cdc42a4
Eric Franz <efranz@osc.edu>
2020-06-07 21:50:39 -0400

    comment clarifying alignment script

--- pipeline/align_sequences.sh
+++ pipeline/align_sequences.sh
@@ -6,7 +6,6 @@ set -o pipefail
 # $1 is path to fasta file ending in .fa
 fasta_file=$1
 fasta_file_aligned="${fasta_file%.fa}.afa"
-fasta_tmp="$TMPDIR/$(basename $fasta_file_aligned)"
 
 function remove_newlines_in_fasta {
   ruby -ne 'print $_[0] == ">" ? "\n#{$_}" : $_.sub("\n","")' | ruby -e 'puts $stdin.read.strip'
@@ -20,5 +19,11 @@ function trim {
   ../bin/trimal -in $1 -resoverlap 0.85 -seqoverlap 50 -gt 0.15
 }
 
+# trimal was throwing errors when doing in a single pipeline:
+#
+#     align $fasta_file |  trim /dev/stdin > $fasta_file_aligned
+#
+# so below uses an intermediate file to ensure alignment completes before trimal begins
+fasta_tmp="$TMPDIR/$(basename $fasta_file_aligned)"
 align $fasta_file > $fasta_tmp
 trim $fasta_tmp | remove_newlines_in_fasta > $fasta_file_aligned
