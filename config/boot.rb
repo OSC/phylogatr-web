61f7e966d0de9a680f967d53025914dd592033d6
Eric Franz <efranz@osc.edu>
2020-06-16 09:03:33 -0400

    enable using app from Owens and web nodes at same time

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+require 'socket'
+if Socket.gethostname.start_with?("o")
+  # Owens
+  ENV['BUNDLE_IGNORE_CONFIG'] = "1"
+  ENV['BUNDLE_PATH'] = "vendor/bundle-owens"
+  puts "using vendor/bundle-owens and ignoring bundle config"
+end
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 # load dotenv files before "before_configuration" callback
