0c2426642c587323c68a630f7ba159b31ccacbe1
Eric Franz <efranz@osc.edu>
2021-05-13 21:44:39 -0400

    avoid newlines in serialization

--- app/models/serialize.rb
+++ app/models/serialize.rb
@@ -2,10 +2,10 @@ require 'base64'
 
 class Serialize
   def self.to_str(obj)
-    Base64.encode64(Marshal.dump(obj))
+    Base64.strict_encode64(Marshal.dump(obj))
   end
 
   def self.from_str(str)
-    Marshal.load(Base64.decode64(str))
+    Marshal.load(Base64.strict_decode64(str))
   end
 end
--- test/models/batch_search_results_test.rb
+++ test/models/batch_search_results_test.rb
@@ -5,4 +5,16 @@ class BatchSearchResultsTest < ActiveJob::TestCase
     params = { :taxon_class => 'Reptilia'}
     assert_equal params, BatchSearchResults.deserialize_params(BatchSearchResults.new(params).serialize_params)
   end
+
+  test "serialization doesn't add newlines" do
+    params = {
+      :southwest_corner_latitude => 1.111,
+      :southwest_corner_longitude => 1.111,
+      :northeast_corner_latitude => 1.111,
+      :northeast_corner_longitude =>1.111,
+      :results_format => 'tar',
+      :taxon_class => 'Reptilia'
+    }
+    refute BatchSearchResults.new(params).serialize_params.include?("\n")
+  end
 end
