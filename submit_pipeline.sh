3ac120ab7e214941ed1cd7917c2d778207de356e
Eric Franz <efranz@osc.edu>
2021-05-22 14:27:59 -0400

    add more steps to submit_pipeline.sh

--- submit_pipeline.sh
+++ submit_pipeline.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PROJ=/fs/project/PAS1604/pipeline/dev
+cp $PROJ/stage-1/genes.tar.gz $PROJ/genes.tar.gz
+
 id=$(sbatch populate_database.pbs)
 id=$(sbatch --dependency=afterany:${id##* } filter_bold_records.pbs)
 id=$(sbatch --dependency=afterany:${id##* } add_bold_records.pbs)
@@ -7,3 +10,7 @@ id=$(sbatch --dependency=afterany:${id##* } add_bold_records.pbs)
 id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterany:${id##* } align.pbs)
 id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
+id=$(sbatch --dependency=afterany:${id##* } clean_database.pbs)
+id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
+id=$(sbatch --dependency=afterany:${id##* } report_species_metrics.pbs)
+id=$(sbatch --dependency=afterany:${id##* } update_alignment_cache.pbs)
