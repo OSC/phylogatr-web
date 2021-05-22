9d6569d2dc3ba0cffc16825d14aa84adf94e811f
Eric Franz <efranz@osc.edu>
2021-05-22 14:31:30 -0400

    move assert_files_same up to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,4 +15,20 @@ class ActiveSupport::TestCase
   def mock_data(path)
     Rails.root.join('test', 'data', path)
   end
+
+  def compare_file_sorted(a, b)
+    # copy impl of FileUtils.copmare_file with addition of sorting
+    return false unless File.size(a) == File.size(b)
+    File.open(a, 'rb') {|fa|
+      File.open(b, 'rb') {|fb|
+        return fa.readlines.sort.join("\n") == fb.readlines.sort.join("\n")
+      }
+    }
+  end
+
+  def assert_files_same(a, b)
+    a = a.to_s
+    b = b.to_s
+    assert FileUtils.compare_file(a, b), (compare_file_sorted(a, b) ? "#{a} and #{b} sort order differs" : "#{a} and #{b} content differs")
+  end
 end
