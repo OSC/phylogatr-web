44b46a9ada0db8be1b23e3492e6e8fb21067a700
Eric Franz <efranz@osc.edu>
2021-05-24 09:15:12 -0400

    add new gbif genbank linker job script

---
+++ genes.rb
@@ -0,0 +1,16 @@
+require 'csv'
+require 'fileutils'
+
+# first remove all the .fa files
+# `find . -name "*.fa" -type f -delete`
+Dir.chdir ARGV.first if ARGV.first
+
+CSV.new(STDIN, col_sep: "\t").each do |row|
+  path = row[0]
+  accession = row[1]
+  sequence = row[7]
+  gbif_id = row[8]
+
+  FileUtils.mkdir_p(File.dirname(path))
+  File.write(path + ".fa", ">#{accession}_#{gbif_id}\n#{sequence}\n", mode: "a+")
+end
