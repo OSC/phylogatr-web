7203c37344d998750c181b16fa3308b4e50ec480
Eric Franz <efranz@osc.edu>
2021-05-25 07:53:01 -0400

    clean db prior to doing alignments

--- submit_pipeline.sh
+++ submit_pipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: add the sbatch pipeline.new.pbs job after renaming that link_gbif_to_genbank.pbs
+
 export PROJ=/fs/project/PAS1604/pipeline/dev
 cp $PROJ/stage-1/genes.tar.gz $PROJ/genes.tar.gz
 
@@ -8,9 +10,9 @@ id=$(sbatch --dependency=afterany:${id##* } filter_bold_records.pbs)
 id=$(sbatch --dependency=afterany:${id##* } add_bold_records.pbs)
 
 id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
-id=$(sbatch --dependency=afterany:${id##* } align.pbs)
-id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterany:${id##* } clean_database.pbs)
 id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
-id=$(sbatch --dependency=afterany:${id##* } report_species_metrics.pbs)
+id=$(sbatch --dependency=afterany:${id##* } align.pbs)
+id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterany:${id##* } update_alignment_cache.pbs)
+id=$(sbatch --dependency=afterany:${id##* } report_species_metrics.pbs)
