b4c4c2709e98761d3afbefd58610c5cd81bb38bb
Eric Franz <efranz@osc.edu>
2021-05-25 21:50:13 -0400

    add fixme comment about failing alignment jobs

--- submit_pipeline.sh
+++ submit_pipeline.sh
@@ -19,6 +19,13 @@ id=$(sbatch --dependency=afterok:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterok:${id##* } clean_database.pbs)
 id=$(sbatch --dependency=afterok:${id##* } update_species_metrics.pbs)
 
+# FIXME: if executing this job alignment without TIMEOUT and no genes.db cache file this job will likely exceed
+# the walltime and fail; since the copy back to project is done at the end,
+# if the walltime kills an alignment job all work is lost
+#
+# consider adding logic here to determine if alignment cache is available,
+# if not run align.pbs three times, first with a timeout of 10m, second with 60m and finally without a timeout,
+# copying results back in between; or add logic here and submit align.pbs several times with different timeouts
 id=$(sbatch --dependency=afterok:${id##* } align.pbs)
 id=$(sbatch --dependency=afterok:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterok:${id##* } update_alignment_cache.pbs)
