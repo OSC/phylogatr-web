e81c17eb264413ed84715a38f08ab337a011bf91
Eric Franz <efranz@osc.edu>
2021-05-25 21:43:57 -0400

    use WORKDIR for all pipeline jobs
    1. the link_gbif_with_genbank.pbs job now generates genes.tsv, gbif.tsv and genes.tar.gz in WORKDIR
       WORKDIR defaults to /fs/project/PAS1604/pipeline/dev but if set to different path in environment
       the different path will be used
    2. link_gbif_with_genbank.pbs doesn't do anything if the output files already exist in WORKDIR
    3. link_gbif_with_genbank.pbs added to submit_pipeline.sh as first step
    
    So now you can do something like WORKDIR=/some/other/path ./submit_pipeline.sh and it should place all the files in WORKDIR

--- submit_pipeline.sh
+++ submit_pipeline.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# TODO: add the sbatch pipeline.new.pbs job after renaming that link_gbif_to_genbank.pbs
+export WORKDIR=${WORKDIR:-/fs/project/PAS1604/pipeline/dev}
 
-export PROJ=/fs/project/PAS1604/pipeline/dev
-cp $PROJ/stage-1/genes.tar.gz $PROJ/genes.tar.gz
+# creates genes.tsv, gbif.tsv, genes.tar.gz
+id=$(sbatch link_gbif_with_genbank.pbs)
 
-id=$(sbatch populate_database.pbs)
+# populates database from gbif.tsv
+id=$(sbatch --dependency=afterany:${id##* } populate_database.pbs)
+
+# creates bold.tsv
 id=$(sbatch --dependency=afterany:${id##* } filter_bold_records.pbs)
+
+# populates database from bold.tsv and adds new files or sequences to existing files in genes.tar.gz
 id=$(sbatch --dependency=afterany:${id##* } add_bold_records.pbs)
 
 id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterany:${id##* } clean_database.pbs)
 id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
+
 id=$(sbatch --dependency=afterany:${id##* } align.pbs)
 id=$(sbatch --dependency=afterany:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterany:${id##* } update_alignment_cache.pbs)
+
 id=$(sbatch --dependency=afterany:${id##* } report_species_metrics.pbs)
