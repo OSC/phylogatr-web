497bf24d46b825c1b539d0ccc24693b73613e17d
Jeff Ohrstrom <johrstrom@osc.edu>
2022-01-04 13:04:11 -0500

    use sbatch files in submit script

--- submit_pipeline.sh
+++ submit_pipeline.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-export WORKDIR=${WORKDIR:-/fs/project/PAS1604/pipeline/dev}
+if [ ! -f "$PWD/env" ]; then
+  echo '$PWD needs to be in the same directory as this file'
+  exit 1
+else
+  source "$PWD/env"
+fi
+
 echo "using workdir: $WORKDIR"
 
 # creates genes.tsv, gbif.tsv, genes.tar.gz
-id=$(sbatch link_gbif_with_genbank.pbs)
+id=$(sbatch link_gbif_with_genbank.sbatch)
 
 # populates database from gbif.tsv
-id=$(sbatch --dependency=afterok:${id##* } populate_database.pbs)
+id=$(sbatch --dependency=afterok:${id##* } populate_database.sbatch)
 
 # creates bold.tsv
-id=$(sbatch --dependency=afterok:${id##* } filter_bold_records.pbs)
+id=$(sbatch --dependency=afterok:${id##* } filter_bold_records.sbatch)
 
 # populates database from bold.tsv and adds new files or sequences to existing files in genes.tar.gz
-id=$(sbatch --dependency=afterok:${id##* } add_bold_records.pbs)
+id=$(sbatch --dependency=afterok:${id##* } add_bold_records.sbatch)
 
 id=$(sbatch --dependency=afterok:${id##* } update_species_metrics.pbs)
 id=$(sbatch --dependency=afterok:${id##* } clean_database.pbs)
